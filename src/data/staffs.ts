01361b3694dbe5455405dc5ddab21f33841230e1
rtang03 <7686266+rtang03@users.noreply.github.com>
2022-10-28 15:38:32 +0800

    add staff account

--- src/data/staffs.ts
+++ src/data/staffs.ts
@@ -1,4 +1,5 @@
 export const staffs = [
   '0xc93b8F86c949962f3B6D01C4cdB5fC4663b1af0A', // mychrome test account 0
+  '0x1AAbF1c8006a22D67dd0d93595652d108e910a08', // mychrome test account 1
   '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' // hardhat account 0
 ];
